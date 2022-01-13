3ed6c70b4c3f25fb0bd44e4d26ffb819a0b32ffe
HieuLT-AVISPL <HT15018@avispl.com>
2022-01-13 15:02:47 +0700

    Feature syal-702 and github actions

---
+++ src/test/java/com/avispl/symphony/dal/avdevices/encoderdecoder/haivision/x4encoder/HaivisionX4EncoderCommunicatorTest.java
@@ -0,0 +1,25 @@
+/*
+ *  * Copyright (c) 2022 AVI-SPL, Inc. All Rights Reserved.
+ */
+package com.avispl.symphony.dal.avdevices.encoderdecoder.haivision.x4encoder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ *
+ * @author Ivan
+ * @since 1.0.0
+ */
+
+public class HaivisionX4EncoderCommunicatorTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
